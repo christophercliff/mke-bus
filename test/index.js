91c2df3bb2ea7842148f922f2b30bd888470142f
Christopher Cliff <chris@percolate.com>
2014-05-18 17:19:06 -0500

    Added stops and directions.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,8 @@
+var _ = require('underscore')
+var async = require('async')
+var fs = require('fs')
+var path = require('path')
+var proxy = require('../lib/proxy')
 var qs = require('querystring').stringify
 var server = require('../')
 var supertest = require('supertest')
@@ -16,7 +21,7 @@ describe('/get/time', function(){
             .expect(200)
             .end(function(err, res){
                 if (err) throw err
-                shouldBeHapi(res.body)
+                shouldBeHapiSuccess(res.body)
                 return done()
             })
     })
@@ -25,10 +30,15 @@ describe('/get/time', function(){
 
 describe('/get/all/vehicles/where', function(){
 
-    it('should return 400', function(done){
+    it('should return 400 for bad requests', function(done){
         supertest(HOST)
             .get('/get/all/vehicles/where')
-            .expect(400, done)
+            .expect(400)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiFailure(res.body)
+                return done()
+            })
     })
 
     it('should get the vehicles', function(done){
@@ -39,7 +49,7 @@ describe('/get/all/vehicles/where', function(){
             .expect(200)
             .end(function(err, res){
                 if (err) throw err
-                shouldBeHapi(res.body)
+                shouldBeHapiSuccess(res.body)
                 return done()
             })
     })
@@ -48,13 +58,24 @@ describe('/get/all/vehicles/where', function(){
 
 describe('/get/vehicle/called/{vehicle_id}', function(){
 
+    it('should return 502 for bad gateway', function(done){
+        supertest(HOST)
+            .get('/get/vehicle/called/abc')
+            .expect(502)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiFailure(res.body)
+                return done()
+            })
+    })
+
     it('should get the vehicle', function(done){
         supertest(HOST)
-            .get('/get/vehicle/called/111')
+            .get('/get/vehicle/called/5115')
             .expect(200)
             .end(function(err, res){
                 if (err) throw err
-                shouldBeHapi(res.body)
+                shouldBeHapiSuccess(res.body)
                 return done()
             })
     })
@@ -69,18 +90,104 @@ describe('/get/all/routes', function(){
             .expect(200)
             .end(function(err, res){
                 if (err) throw err
-                shouldBeHapi(res.body)
+                shouldBeHapiSuccess(res.body)
                 return done()
             })
     })
 
 })
 
-function shouldBeHapi(body) {
-    return
+describe('/get/all/directions', function(){
+
+    it('should return 400 for bad requests', function(done){
+        supertest(HOST)
+            .get('/get/all/directions/where')
+            .expect(400)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiFailure(res.body)
+                return done()
+            })
+    })
+
+    it('should get the directions', function(done){
+        supertest(HOST)
+            .get(['/get/all/directions/where', qs({
+                route_id: '21'
+            })].join('?'))
+            .expect(200)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiSuccess(res.body)
+                return done()
+            })
+    })
+
+})
+
+describe('/get/all/stops/where', function(){
+
+    it('should return 400 for bad requests', function(done){
+        supertest(HOST)
+            .get('/get/all/stops/where')
+            .expect(400)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiFailure(res.body)
+                return done()
+            })
+    })
+
+    it('should get the stops', function(done){
+        supertest(HOST)
+            .get(['/get/all/stops/where', qs({
+                route_id: '21',
+                direction: 'EAST'
+            })].join('?'))
+            .expect(200)
+            .end(function(err, res){
+                if (err) throw err
+                shouldBeHapiSuccess(res.body)
+                return done()
+            })
+    })
+
+})
+
+describe('proxy.getGatewayError()', function(){
+
+    it('should parse error messages', function(done){
+        var errors = _.map({
+            './fixtures/getvehicles-bad-request.xml': 'No data found for parameter',
+            './fixtures/gettime-bad-request.xml': 'Invalid API access key supplied'
+        }, function(message, path){
+            return { message: message, path: path }
+        })
+        async.each(errors, function(n, callback){
+            fs.readFile(path.resolve(__dirname, n.path), { encoding: 'utf8' }, function(err, xml){
+                if (err) throw err
+                proxy.toData(xml, function(err, data){
+                    if (err) throw err
+                    proxy.getGatewayError(data).should.equal(n.message)
+                    return callback(null)
+                })
+            })
+        }, done)
+    })
+
+})
+
+function shouldBeHapiSuccess(body) {
     body.should.be.an.instanceof(Object)
-    body.should.have.property('this')
+    body.should.have.property('this', 'succeeded')
     body.should.have.property('by')
     body.should.have.property('the')
     body.should.have.property('with')
+}
+
+function shouldBeHapiFailure(body) {
+    body.should.be.an.instanceof(Object)
+    body.should.have.property('this', 'failed')
+    body.should.have.property('with')
+    body.should.have.property('because')
 }
